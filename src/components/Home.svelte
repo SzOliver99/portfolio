fd8b2ee955eb15fcf7087c8fa5ec15022e9b1aed
Oliver Szvetnyik <oliver.szvetnyik@gmail.com>
2024-09-11 19:03:38 +0200

    modify

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -4,8 +4,8 @@
 
 <!-- <h1 class="title">Oliver Szvetnyik</h1>
 <p>Portfolio</p> -->
-<h1>Ray Space</h1>
-<p>Hi there, I'm Ray. I'm a <b>Software Engineer at Accenture</b> where I work on innovative e-government projects.</p>
+<h1>Oliver Szvetnyik</h1>
+<p>Hi there, I'm Oliver. I'm a <b>Software Engineer at Accenture</b> where I work on innovative e-government projects.</p>
 <p>I use this place to explore new ideas, experiment with different technologies, and to deconstruct complex tech concepts. If you're interested in my thoughts on something, please let me know!</p>
 
 <style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import '../app.css';
 	import SidenavItem from '../components/SidenavItem.svelte';
+	import rust_logo from '../lib/images/rust-logo.png';
 </script>
 
 <div class="app">
 	<div class="sidebar">
 		<ul class="sidenav-list">
+			<picture>
+				<source srcset={rust_logo} type="image/png" />
+                <img src={rust_logo} alt="Logo" />
+			</picture>
 			<SidenavItem id="homeButton" text="home" href="/portfolio"></SidenavItem>
 			<SidenavItem id="aboutButton" text="about" href="/portfolio/about"></SidenavItem>
 			<SidenavItem id="projectsButton" text="projects" href="/portfolio/projects"></SidenavItem>
@@ -30,13 +35,15 @@
 		padding: 10px;
 	}
 
+	picture img {
+	}
+
 	.sidenav-list {
 		display: flex;
 		flex-direction: column;
 		gap: 20px;
 		list-style-type: none;
 		text-align: right;
-		padding-top: 45px;
 	}
 
 	.content {
